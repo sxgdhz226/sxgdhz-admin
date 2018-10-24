82426e12b67fefbcbfad5e5dbf6ae2e40bc4d69b
sxgdhz226 <1600328315@qq.com>
2018-10-24 17:30:12 +0800

    每天进步一点点

---
+++ springboot-sxgdhz/sxgdhz/sxgdhz-api/src/main/java/com/RuoYi/ApiController.java
@@ -0,0 +1,18 @@
+package com.RuoYi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ApiController
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(ApiController.class);
+    }
+}
